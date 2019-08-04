26861415be6e0b8a8ff0127735228b84735cf04a
Tom Victor <vjtomvictor@icloud.com>
2019-08-04 12:36:10 +0530

    django project and apps added

---
+++ core/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=50, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class ImoCalculation(models.Model):
+	name = models.CharField(max_length=50, blank=False)
+	imo_param_a = models.CharField(max_length=50, blank=True, null=True)
+	imo_param_b = models.CharField(max_length=50, blank=True, null=True)
+	imo_param_c = models.CharField(max_length=50, blank=True, null=True)
+	
+	def __str__(self):
+		return self.name
+
+class DnvglCalculation(models.Model):
+	name = models.CharField(max_length=50, blank=False)
+	dnvgl_param1 = models.CharField(max_length=50, blank=True, null=True)
+	dnvgl_param2 = models.CharField(max_length=50, blank=True, null=True)
+	dnvgl_param3 = models.CharField(max_length=50, blank=True, null=True)
+	def __str__(self):
+		return self.name
