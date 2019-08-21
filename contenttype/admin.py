9332f6a54470f6aff265b49b9e1cce471672ba90
tom victor <tom.victor@admaren.com>
2019-08-21 14:24:06 +0530

    models updated

--- contenttype/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from contenttype.models import *
-# Register your models here.
-
-admin.site.register(Project)
-admin.site.register(ImoCalculation)
-admin.site.register(DnvglCalculation)
