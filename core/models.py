4648bfcbe99aba3786ef992edb4c37a900a7927c
tom victor <tom.victor@admaren.com>
2019-08-21 14:25:41 +0530

    model update

--- core/models.py
+++ core/models.py
@@ -7,7 +7,7 @@ from django.contrib.contenttypes.models import ContentType
 
 
 class GeoBlocker(models.Model):
-    name = models.CharField(max_length=150, blank=True, null=True)
+    country = models.CharField(max_length=150, blank=True, null=True)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
